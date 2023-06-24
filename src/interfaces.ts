3298a0a5d286c37bc4ed28b818be1d81db6fa4c3
KajiyamaVK <victor.kajiyama@gmail.com>
2023-06-24 11:24:32 -0300

    refactor: Adds more public interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,18 @@
+interface IFilePath {
+  path: string;
+  extension: string;
+}
+
 export interface ICharacters {
   id: number;
   name: string;
   description: string;
+  thumbnail: IFilePath;
+}
+
+export interface ISearchResult {
+  count: number;
+  offset: number;
+  total: number;
+  results: ICharacters[];
 }

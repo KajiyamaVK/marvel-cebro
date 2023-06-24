233d8d11bf5dbfac0f5d869e236e221346152de8
KajiyamaVK <victor.kajiyama@gmail.com>
2023-06-24 11:23:43 -0300

    feat: Treats the data return

--- src/functions.ts
+++ src/functions.ts
@@ -1,6 +1,11 @@
-export async function getCharacters() {
+import { ISearchResult } from "./interfaces";
+import { ICharacters } from "./interfaces";
+
+export async function getCharacters(
+  offset: number = 0
+): Promise<ISearchResult> {
   const { data } = await fetch(
-    `https://gateway.marvel.com:443/v1/public/characters?apikey=${process.env.NEXT_PUBLIC_APIKEY}`,
+    `https://gateway.marvel.com:443/v1/public/characters?apikey=${process.env.NEXT_PUBLIC_APIKEY}&offset=${offset}`,
     {
       method: "GET",
       headers: {
@@ -9,5 +14,22 @@ export async function getCharacters() {
     }
   ).then((response) => response.json());
 
-  return data;
+  const searchResult: ISearchResult = {
+    count: data.count,
+    offset: data.offset,
+    total: data.total,
+    results: data.results.map((character: ICharacters) => ({
+      id: character.id,
+      name: character.name,
+      description: character.description,
+      thumbnail: {
+        path: character.thumbnail.path,
+        extension: character.thumbnail.extension,
+      },
+    })),
+  };
+
+  console.log(searchResult);
+
+  return searchResult;
 }
